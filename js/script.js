6f2961a4e49b1f437e0cb3d637c7015ba5f7c5e0
Walid Ait Moussa <walid.aitmoussa@edu.uiz.ac.ma>
2024-12-05 10:16:18 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 const textarea = document.getElementById('binarycode');
-textarea.addEventListener('input', function(event) {    
+
+function set_seperate(){
     // Get the current cursor position
-    const start = this.selectionStart;
+    const start = textarea.selectionStart;
 
     // Filter out spaces and allow only valid hex characters (0-9, A-F, a-f)
-    const valueWithoutSpaces = this.value.replace(/ /g, '').replace(/[^0-9A-Fa-f]/g, '');
+    const valueWithoutSpaces = textarea.value.replace(/ /g, '').replace(/[^0-9A-Fa-f]/g, '');
     
     // Format input by adding a space after every two characters
     const formattedValue = valueWithoutSpaces.replace(/(.{1,4})/g, '$1 ').trim();
 
     // Update the textarea with the new formatted value
-    this.value = formattedValue;
+    textarea.value = formattedValue;
 
     // Calculate the new cursor position
 
     const newCursorPosition = (formattedValue[start-1]==' ')?start+1:start;
-    this.setSelectionRange(newCursorPosition, newCursorPosition); // Set the cursor position
+    textarea.setSelectionRange(newCursorPosition, newCursorPosition); // Set the cursor position
+}
+
+textarea.addEventListener('input', function(event) {    
+    set_seperate();
 });
 
 const computingBtn = document.getElementById('computingBtn');
@@ -25,7 +30,8 @@ computingBtn.addEventListener('click', function() {
     const value = textarea.value.replace(/ /g, '');
     const com = new Computer(value);
     com.computing(debbugCHBX.checked);
-    computingOut.innerText = com.m_Output;
+    computingOut.innerText = "";
+    computingOut.innerHTML = com.m_Output;
 });
 
 const assembleBtn = document.getElementById('assembleBtn');
@@ -33,6 +39,32 @@ const assemblyCode = document.getElementById("assembly");
 assembleBtn.addEventListener("click", function () {
     const assembler = new Assembler(assemblyCode.value);
     assembler.assembling();
+    textarea.value = "";
     textarea.value = assembler.m_Out;
+    set_seperate();
+});
+
+const progcode = document.getElementById('progcode')
+const compileBtn = document.getElementById('compilingBtn')
+compileBtn.addEventListener('click', function(){
+    const compiler = new Compiler(progcode.value);
+    compiler.compiling();
+    assemblyCode.value = "";
+    assemblyCode.value = compiler.m_Out;
+    
+});
+
+const progcode1 = document.getElementById('progcode');
+const runningBtn = document.getElementById('runningBtn');
+runningBtn.addEventListener('click', function(){
+    const compiler = new Compiler(progcode1.value);
+    compiler.compiling();
+    const assembler = new Assembler(compiler.m_Out);
+    assembler.assembling();
+    const value = assembler.m_Out.replace(/ /g, '');
+    const com = new Computer(value);
+    com.computing(debbugCHBX.checked);
+    computingOut.innerHTML = com.m_Output;
+    
 });
 
